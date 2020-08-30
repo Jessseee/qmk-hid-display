e5c750ade697f2e2fdbc78d99bda8dc950835a09
Brandon Wang <brandonwang@me.com>
2020-08-30 05:12:17 -0700

    simplify weather by using weather-js

--- screens/weather.js
+++ screens/weather.js
@@ -1,9 +1,7 @@
-// weather page
-// logic from https://github.com/BlankSourceCode/qmk-hid-display
 'use strict';
 
 const { LoopingScreen } = require('./screen.js');
-const request = require('request');
+const weather = require('weather-js');
 
 class WeatherScreen extends LoopingScreen {
   init() {
@@ -14,59 +12,35 @@ class WeatherScreen extends LoopingScreen {
     this.temp = '';
     this.high = '';
     this.rain = '';
-  }
 
-  getWeather() {
-    // Regex's for reading out the weather info from the yahoo page
-    const tempRegex = /"temperature":({[^}]+})/;
-    const condRegex = /"conditionDescription":"([^"]+)"/;
-    const rainRegex = /"precipitationProbability":([^,]+),/;
-    return new Promise((resolve) => {
-      request(`https://www.yahoo.com/news/weather/united-states/seattle/seattle-2490383`, (err, res, body) => {
-        const weather = {};
-        const temp = tempRegex.exec(body);
-        if (temp && temp.length > 1) {
-          weather.temp = JSON.parse(temp[1]);
-        }
-
-        const cond = condRegex.exec(body);
-        if (cond && cond.length > 1) {
-          weather.desc = cond[1];
-        }
-
-        const rain = rainRegex.exec(body);
-        if (rain && rain.length > 1) {
-          weather.rain = rain[1];
-        }
-        resolve(weather);
-      });
-    });
+    this.location = 'Santa Monica, CA';
+    this.degreeType = 'F';
   }
 
   updateScreen() {
     this.screen = [
       this.screenScroll(this.description),
-      'temp: ' + this.temp,
-      'high: ' + this.high,
-      'rain: ' + this.rain
+      'temp: ' + this.temp + '\u0009',
+      'high: ' + this.high + '\u0009',
+      'rain: ' + this.rain + '%'
     ]
     super.updateScreen();
   }
 
   update() {
-    // Get the current weather for Seattle
-    this.getWeather().then((weather) => {
-      if (weather && weather.temp && weather.desc && weather.rain) {
-        // update info
-        this.description = weather.desc;
-        this.temp = weather.temp.now + '\u0009';
-        this.high = weather.temp.high + '\u0009';
-        this.rain = weather.rain + '%';
-
-        // Create the new screen
-        this.updateScreen();
+    weather.find({search: this.location, degreeType: this.degreeType},
+      (err, result) => {
+        if (err) {
+          this.log('Error: ' + err);
+        }
+        this.description = '' + result[0].current.skytext;
+        this.temp = result[0].current.temperature;
+        // forecast[1] is today
+        this.high = result[0].forecast[1].high;
+        this.rain = result[0].forecast[1].precip;
       }
-    });
+    );
+    this.requestUpdateScreen();
   }
 }
 
