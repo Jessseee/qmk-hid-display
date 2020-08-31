272701b04e86608ad329a7fbcfb32af75c8f2c0c
Brandon Wang <brandonwang@me.com>
2020-08-31 00:51:30 -0700

    move original keyboardhid code to another file

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const hid = require('node-hid');
 const nconf = require('nconf');
 const fs = require('fs');
 const crypto = require('crypto');
 const { app, BrowserWindow, Menu, Tray, session } = require('electron');
 const ScreenManager = require('./screenmanager.js');
+const keyboardHid = require('./keyboardhid.js');
 
-let screenManager = null;
 // config
 // get a secret or create one. hacky but whatever 
 let secret = '';
@@ -32,6 +31,8 @@ nconf.argv()
   .file('./config.json')
   .defaults({
     'keyboardName': 'SPIN',
+    'keyboardId': '0x61',
+    'keyboardPage': '0xFF60',
     'spotifyClientId': '<spotify client id>',
     'spotifyClientSecret': '<spotify client secret>',
     'weatherLocation': 'Santa Monica, CA',
@@ -40,98 +41,10 @@ nconf.argv()
   });
 nconf.load();
 
-// Keyboard info
-const KEYBOARD_NAME = nconf.get('keyboardName');
-const KEYBOARD_USAGE_ID =  0x61;
-const KEYBOARD_USAGE_PAGE = 0xFF60;
+// Keyboard info;
 
-let keyboard = null;
-let screenBuffer = null;
-let screenLastUpdate = null;
 
-// Helper function to wait a few milliseconds using a promise
-function wait(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  })
-}
-
-async function sendToKeyboard(screen) {
-  // If we are already buffering a screen to the keyboard just quit early.
-  // Or if there is no update from what we sent last time.
-  if (screenBuffer || screenLastUpdate === screen) {
-    return;
-  }
-
-  screenLastUpdate = screen;
-
-  // Convert the screen string into raw bytes
-  screenBuffer = [];
-  for (let i = 0; i < screen.length; i++) {
-    screenBuffer.push(screen.charCodeAt(i));
-  }
-
-  // Split the bytes into 4 lines that we will send one at a time
-  // This is to prevent hitting the 32 length limit on the connection
-  const lines = [];
-  lines.push([0].concat(screenBuffer.slice(0, 21)));
-  lines.push([0].concat(screenBuffer.slice(21, 42)));
-  lines.push([0].concat(screenBuffer.slice(42, 63)));
-  lines.push([0].concat(screenBuffer.slice(63, 84)));
-
-  // Loop through and send each line after a small delay to allow the
-  // keyboard to store it ready to send to the slave side once full.
-  let index = 0;
-  for (const line of lines) {
-    try {
-      keyboard.write(line);
-      await wait(10);
-    } catch {
-      keyboard = null;
-      initializeConnection();
-    }
-  }
-
-  // We have sent the screen data, so clear it ready for the next one
-  screenBuffer = null;
-}
-
-function initializeConnection() {
-  // If we don't have a connection to a keyboard yet, look now
-  if (screenManager && !keyboard) {
-    // Search all devices for a matching keyboard
-    const devices = hid.devices();
-    for (const d of devices) {
-      if (d.product === KEYBOARD_NAME && d.usage === KEYBOARD_USAGE_ID && d.usagePage === KEYBOARD_USAGE_PAGE) {
-        // Create a new connection and store it as the keyboard
-        keyboard = new hid.HID(d.path);
-        console.log(`Keyboard connection established.`);
-
-        // Listen for data from the keyboard which indicates the screen to show
-        keyboard.on('data', (e) => {
-          // Check that the data is a valid screen index and update the current one
-          const requestedScreen = e[0] - 1;
-          if (requestedScreen >= 0 && requestedScreen < screenManager.numScreens) {
-            screenManager.setActiveScreen(requestedScreen);
-            console.log(`Activating screen: ${screenManager.getActiveScreen().name}`);
-          }
-        });
-
-        // On the initial connection write our special sequence
-        // 1st byte - unused and thrown away on windows see bug in node-hid
-        // 2nd byte - 1 to indicate a new connection
-        // 3rd byte - number of screens the keyboard can scroll through
-        keyboard.write([0, 1, screenManager.numScreens]);
-        break;
-      }
-    }
-  }
-  else {
-    setInterval(initializeConnection, 1000);
-    return;
-  }
-}
-
+let screenManager = null;
 let tray = null;
 
 function updateContextMenu() {
@@ -152,8 +65,12 @@ function createTray () {
   updateContextMenu();
   screenManager = new ScreenManager([tray, nconf, session],
     updateContextMenu,
-    () => { sendToKeyboard(screenManager.getActiveOutput()); });
-  initializeConnection();
+    () => { keyboardHid.sendToKeyboard(screenManager.getActiveOutput()); });
+  keyboardHid.init(nconf.get('keyboardName'),
+    parseInt(nconf.get('keyboardId')),
+    parseInt(nconf.get('keyboardPage')),
+    screenManager);
+  keyboardHid.initConnection();
 }
 
 app.on('window-all-closed', () => {
---
+++ keyboardhid.js
@@ -0,0 +1,109 @@
+'use strict';
+
+// From https://github.com/BlankSourceCode/qmk-hid-display
+const hid = require('node-hid');
+
+let keyboardName = '';
+let keyboardId = 0;
+let keyboardPage = 0;
+
+let keyboard = null;
+let screenBuffer = null;
+let screenLastUpdate = null;
+let screenManager = null;
+
+// Helper function to wait a few milliseconds using a promise
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  })
+}
+
+function init(inKeyboardName, inKeyboardId, inKeyboardPage, inScreenManager) {
+  keyboardName = inKeyboardName;
+  keyboardId = inKeyboardId;
+  keyboardPage = inKeyboardPage;
+  screenManager = inScreenManager;
+}
+
+async function sendToKeyboard(screen) {
+  // If we are already buffering a screen to the keyboard just quit early.
+  // Or if there is no update from what we sent last time.
+  if (screenBuffer || screenLastUpdate === screen) {
+    return;
+  }
+
+  screenLastUpdate = screen;
+
+  // Convert the screen string into raw bytes
+  screenBuffer = [];
+  for (let i = 0; i < screen.length; i++) {
+    screenBuffer.push(screen.charCodeAt(i));
+  }
+
+  // Split the bytes into 4 lines that we will send one at a time
+  // This is to prevent hitting the 32 length limit on the connection
+  const lines = [];
+  lines.push([0].concat(screenBuffer.slice(0, 21)));
+  lines.push([0].concat(screenBuffer.slice(21, 42)));
+  lines.push([0].concat(screenBuffer.slice(42, 63)));
+  lines.push([0].concat(screenBuffer.slice(63, 84)));
+
+  // Loop through and send each line after a small delay to allow the
+  // keyboard to store it ready to send to the slave side once full.
+  let index = 0;
+  for (const line of lines) {
+    try {
+      keyboard.write(line);
+      await wait(10);
+    } catch {
+      keyboard = null;
+      initConnection();
+    }
+  }
+
+  // We have sent the screen data, so clear it ready for the next one
+  screenBuffer = null;
+}
+
+function initConnection() {
+  // If we don't have a connection to a keyboard yet, look now
+  if (screenManager && !keyboard) {
+    // Search all devices for a matching keyboard
+    const devices = hid.devices();
+    for (const d of devices) {
+      if (d.product === keyboardName && d.usage === keyboardId && d.usagePage === keyboardPage) {
+        // Create a new connection and store it as the keyboard
+        keyboard = new hid.HID(d.path);
+        console.log(`Keyboard connection established.`);
+
+        // Listen for data from the keyboard which indicates the screen to show
+        keyboard.on('data', (e) => {
+          // Check that the data is a valid screen index and update the current one
+          const requestedScreen = e[0] - 1;
+          if (requestedScreen >= 0 && requestedScreen < screenManager.numScreens) {
+            screenManager.setActiveScreen(requestedScreen);
+            console.log(`Activating screen: ${screenManager.getActiveScreen().name}`);
+          }
+        });
+
+        // On the initial connection write our special sequence
+        // 1st byte - unused and thrown away on windows see bug in node-hid
+        // 2nd byte - 1 to indicate a new connection
+        // 3rd byte - number of screens the keyboard can scroll through
+        keyboard.write([0, 1, screenManager.numScreens]);
+        break;
+      }
+    }
+  }
+  else {
+    setInterval(() => { initConnection() }, 1000);
+    return;
+  }
+}
+
+module.exports = {
+  sendToKeyboard: sendToKeyboard,
+  initConnection: initConnection,
+  init: init
+};
